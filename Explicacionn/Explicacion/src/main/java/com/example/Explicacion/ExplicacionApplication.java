b093c1c4a7873715258e01cf56b346ad60ccd62f
Maximiliano Baranda Carvajal <20243ds175@utez.edu.mx>
2025-12-01 12:26:53 -0600

    Add files via upload
    ex

---
+++ Explicacionn/Explicacion/src/main/java/com/example/Explicacion/ExplicacionApplication.java
@@ -0,0 +1,11 @@
+package com.example.Explicacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExplicacionApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ExplicacionApplication.class, args);
+	}
+}
